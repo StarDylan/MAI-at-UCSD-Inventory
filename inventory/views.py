c9405f0b24d6ada0db3f1ec878f735b76bb43c96
Dylan Starink <dylan@starink.com>
2025-08-20 21:01:05 -0700

    initial

---
+++ inventory/views.py
@@ -0,0 +1,23 @@
+from rest_framework import permissions, viewsets
+
+from inventory.models import Category, Item
+from inventory.serializers import ItemSerializer, CategorySerializer
+
+# Create your views here.
+
+class ItemViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows items to be viewed or edited.
+    """
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows categories to be viewed or edited.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from maiv2!")
+
+
+if __name__ == "__main__":
+    main()
