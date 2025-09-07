355da0dae78bbac0542478550292586c5f0a5da9
Dylan Starink <dylan@starink.com>
2025-09-06 20:10:48 -0700

    get secret key directly

--- backend/mai/settings.py
+++ backend/mai/settings.py
@@ -32,7 +32,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'), parse_comments=True)
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', default=get_random_secret_key())
+SECRET_KEY = os.getenv("SECRET_KEY") or get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
